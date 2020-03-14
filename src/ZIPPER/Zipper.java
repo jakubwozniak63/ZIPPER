a46df413ae4c4615127f9ce3f444d7ac0d3b8118
Jakub Woźniak <jakub.wozniak63@gmail.com>
2020-03-14 16:13:07 +0100

    Zip function added

--- src/ZIPPER/Zipper.java
+++ src/ZIPPER/Zipper.java
@@ -3,8 +3,15 @@ package ZIPPER;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 
 public class Zipper extends JFrame
@@ -171,7 +178,7 @@ public class Zipper extends JFrame
                 }
         private void usuwanieWpisowZListy()
         {
-            int opt = JOptionPane.showConfirmDialog(null, "Czy jesteś pewny, ze chcesz trwale usunąć plik?", "Usuwanie pliku z archiwum", JOptionPane.YES_NO_OPTION);
+            int opt = JOptionPane.showConfirmDialog(null, "Czy jesteś pewny, ze chcesz usunąć plik z listy?", "Usuwanie pliku z listy", JOptionPane.YES_NO_OPTION);
             if (opt == 0)
             {
             int[] tmp = lista.getSelectedIndices();
@@ -186,9 +193,74 @@ public class Zipper extends JFrame
         {
             wybieracz.setCurrentDirectory(new File(System.getProperty("user.dir")));
             wybieracz.setSelectedFile(new File(System.getProperty("user.dir")+File.separator+"mojanazwa.zip"));
-            wybieracz.showDialog(rootPane, "Kompresuj");
+            int tmp = wybieracz.showDialog(rootPane, "Kompresuj");
+            
+            if (tmp == JFileChooser.APPROVE_OPTION)
+            {
+               byte tmpData[] = new byte[BUFFOR];
+               try
+               {
+                ZipOutputStream zOutS = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(wybieracz.getSelectedFile()),BUFFOR));
+
+                for (int i = 0; i < modelListy.getSize(); i++)
+                {
+                    if (!((File)modelListy.get(i)).isDirectory())
+                        zipuj(zOutS, (File)modelListy.get(i), tmpData, ((File)modelListy.get(i)).getPath());
+                    else
+                    {
+                        wypiszSciezki((File)modelListy.get(i));
+                                
+                        for (int j = 0; j < listaSciezek.size(); j++)
+                          zipuj(zOutS, (File)listaSciezek.get(j), tmpData, ((File)modelListy.get(i)).getPath());
+                        
+                        listaSciezek.removeAll(listaSciezek);
+                    }
+                        
+                }
+
+                zOutS.close();
+               }
+               catch(IOException e)
+               {
+                   System.out.println(e.getMessage());
+               }
+            }
         }
-   
+        private void zipuj(ZipOutputStream zOutS, File sciezkaPliku, byte[] tmpData, String sciezkaBazowa) throws IOException
+        {
+            BufferedInputStream inS = new BufferedInputStream(new FileInputStream(sciezkaPliku), BUFFOR);
+
+            zOutS.putNextEntry(new ZipEntry(sciezkaPliku.getPath().substring(sciezkaBazowa.lastIndexOf(File.separator)+1)));
+            
+            int counter;
+            while ((counter = inS.read(tmpData, 0, BUFFOR)) != -1)
+                zOutS.write(tmpData, 0, counter);
+
+
+            zOutS.closeEntry();
+
+            inS.close();            
+        }
+        public static final int BUFFOR = 1024;
+       
+        private void wypiszSciezki(File nazwaSciezki)
+        {
+           String[] nazwyPlikowIKatalogow = nazwaSciezki.list();
+
+           for (int i = 0; i < nazwyPlikowIKatalogow.length; i++)
+           {
+               File p = new File(nazwaSciezki.getPath(), nazwyPlikowIKatalogow[i]);
+
+               if (p.isFile())
+                   listaSciezek.add(p);
+
+               if (p.isDirectory())
+                   wypiszSciezki(new File(p.getPath()));
+
+           }
+        }
+        
+        ArrayList listaSciezek = new ArrayList();
     }
     
     
